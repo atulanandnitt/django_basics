f04631e402e3b2d7b1fb58be362ce8b7460c17e9
atulanandnitt <atul.anand.nitt@gmail.com>
2021-05-22 17:50:38 +0530

    Versioning through URI Path

--- src/base_app/views.py
+++ src/base_app/views.py
@@ -12,6 +12,29 @@ class Summation(Base):
             request_data = request.data
             sol = 0
             print(request_data, type(request_data))
+            a = request_data.get("a", 0)
+            b = request_data.get("b", 0)
+            sol = a+b
+            # for key, val in request_data.items():
+            #     sol += int(val)
+            respose = {"sol": sol}
+            return Response(respose,
+            status = status.HTTP_200_OK,
+            content_type='application/json')
+        except Exception as ex:
+            dummy_respose = dict()
+            return Response(dummy_respose,
+            status = status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content_type='application/json')
+
+
+class SummationGeneric(Base):
+    def post(self, request):
+        # import ipdb; ipdb.set_trace()
+        try:
+            request_data = request.data
+            sol = 0
+            print(request_data, type(request_data))
             for key, val in request_data.items():
                 sol += int(val)
             respose = {"sol": sol}
