a4b43efbf53e4ff4cd892ffecc029dfcb66ec60f
atulanandnitt <atul.anand.nitt@gmail.com>
2021-05-29 13:49:13 +0530

    restructure

--- src/base_app/services/questions_bank/advancedDataStructure/LinkedLists/@singleLL.py
+++ src/base_app/services/questions_bank/advancedDataStructure/LinkedLists/@singleLL.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, val):
+    def __init__(self, val=0):
         self.next = None
         self.data = val
 
@@ -81,8 +81,34 @@ class LinkedList:
     def add_math_ll(self):
         pass
 
+    def odd_even_ll(self) -> Node:
+        # TODO: ???
+        # https://www.youtube.com/watch?v=QcSLh0JtwFk&list=PLYAlGR1wWgUU75tRZfDFEorFstOrRGAoV&index=20
+        # https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3331/
+        for_odd = self.head
+        for_even = self.head
+        temp = self.head
+        odd_head = for_odd
+        even_head = for_even
+        is_odd = True
+        while temp:
+            if is_odd:
+                for_odd.next = temp
+                for_odd = for_odd.next
+                print("displaying the ll: ", end="")
+                self.display()
+            else:
+                for_even.next = temp
+                for_even = for_even.next
+            is_odd = not is_odd
+            temp = temp.next
+        for_even.next = None
+        for_odd.next = even_head.next
+        return odd_head.next
+
 ll = LinkedList()
-list1 = [1,3,5,7,9,11,7]
+# list1 = [1,3,5,7,9,11,7]
+list1 = range(11)
 for item in list1:
     ll.add_node(item)
 
@@ -99,3 +125,7 @@ print("detect_loop(self) : ", ll.detect_loop())
 
 print("remove_loop(self) : ", ll.remove_loop())
 print("after remove_loop: ", ll.display())
+
+
+head = ll.odd_even_ll()
+print("ll.display() after ll.odd_even_ll(): ", ll.display())
